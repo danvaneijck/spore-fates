eaa54db3e17a7422875cc3cad1b68a253fcd9a4f
dan <dan.ve16587@gmail.com>
2025-12-12 16:02:07 +1300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { nodePolyfills } from "vite-plugin-node-polyfills"; // Import this
-import vercelApi from "vite-plugin-vercel-api"; // Import the plugin
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+import vercelApi from "vite-plugin-vercel-api";
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         react(),
         nodePolyfills({
             protocolImports: true,
+            globals: {
+                Buffer: true,
+                global: true,
+                process: true,
+            },
         }),
         vercelApi(),
     ],
+    define: {
+        global: "globalThis",
+    },
     optimizeDeps: {
         exclude: ["lucide-react"],
     },
